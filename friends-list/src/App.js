418e945c310caf3981c11a9b25076f8b5354f3e6
Namit Malasi <namitmalasi@gmail.com>
2020-11-04 02:20:45 +0530

    added files

---
+++ friends-list/src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return <PersonList />;
+};
+
+const PersonList = () => {
+  const people = [
+    {
+      img: 22,
+      name: "john",
+      job: "developer",
+    },
+    {
+      img: 34,
+      name: "bob",
+      job: "designer",
+    },
+    {
+      img: 56,
+      name: "peter",
+      job: "artist",
+    },
+  ];
+  return (
+    <section>
+      <Person person={people[0]} />;
+      <Person person={people[1]}>
+        mdasjdasudhasndjnas d asjdbajsbdsd adjnada dja
+      </Person>
+      ;
+      <Person person={people[2]} />;
+    </section>
+  );
+};
+
+const Person = (props) => {
+  const { img, name, job } = props.person;
+  const { children } = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  return (
+    <div className="person">
+      <img src={url} alt="person-pic"></img>
+      <div>
+        <h4>{name}</h4>
+        <h4>{job}</h4>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default App;
